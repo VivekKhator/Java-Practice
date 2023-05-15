f8e920aa9506c725c2551b98dae172e0f82ed5d3
VivekKhator <vivekkumar14728@gmail.com>
2023-05-15 14:53:19 +0530

    Added code for finding GCD

--- Basic_Problems/FindGCD.java
+++ Basic_Problems/FindGCD.java
@@ -1,8 +1,20 @@
 package Basic_Problems;
+import java.util.Scanner;
 
 public class FindGCD {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int small = a<b?a:b;
         
+        for(int i=small; i>0; i--){
+            if(a%i==0 && b%i==0){
+                System.out.println("GCD: " +i);
+                break;
+            }
+        }
     }
 }
