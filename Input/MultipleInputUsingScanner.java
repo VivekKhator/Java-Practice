06fdddc01b6f821dc1088221ef4b3fe160ccd036
VivekKhator <vivekkumar14728@gmail.com>
2023-05-15 14:41:47 +0530

    Added Multiple Input Using Scanner code

---
+++ Input/MultipleInputUsingScanner.java
@@ -0,0 +1,21 @@
+package Input;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultipleInputUsingScanner {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many input you want to take: ");
+        int n = sc.nextInt();
+        
+        int[] nums = new int[n];
+
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
