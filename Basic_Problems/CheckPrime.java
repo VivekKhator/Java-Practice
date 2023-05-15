b0ce11e1ac640c33a74486d0702953acc89ccb38
VivekKhator <vivekkumar14728@gmail.com>
2023-05-15 15:13:15 +0530

    Added Prime Number code

---
+++ Basic_Problems/CheckPrime.java
@@ -0,0 +1,31 @@
+package Basic_Problems;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+
+    static boolean isPrime(int n){
+
+        for(int i=2; i<n; i++){
+            if(n<=1){
+                return false;
+            }
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(isPrime(n)){
+            System.out.println(n + " is a prime number.");
+        }
+        else{
+            System.out.println(n +" is not a prime number.");
+        }
+    }
+}
