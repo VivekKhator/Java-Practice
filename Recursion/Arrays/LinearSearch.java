94fc4589ca13676c475860f6b8f7d7db6a4c91d6
VivekKhator <vivekkumar14728@gmail.com>
2023-05-30 07:29:12 +0530

    Linear Search using recursion code added

--- Recursion/Arrays/LinearSearch.java
+++ Recursion/Arrays/LinearSearch.java
@@ -3,10 +3,24 @@ package Arrays;
 public class LinearSearch {
 
     static int linearSearch(int[] arr, int t){
-        return helper(arr, t, 0);
+        // return helperFirst(arr, t, 0);
+        return helperLast(arr,t,arr.length-1);
     }
 
-    static int helper(int[] arr, int t, int i){
+    static int helperLast(int[] arr, int t, int l){
+        if(arr[l]==t){
+            return l;
+        }
+
+        if(l==0){
+            return -1;
+        }
+
+        return helperLast(arr, t, l-1);
+
+    }
+
+    static int helperFirst(int[] arr, int t, int i){
 
         if(arr[i]==t){
             return i;
@@ -15,12 +29,12 @@ public class LinearSearch {
             return -1;
         }
 
-        return helper(arr,t,i+1);
+        return helperFirst(arr,t,i+1);
     }
     
     public static void main(String[] args) {
-        int[] arr = {1,3,5,2,7,9,6,8};
-        int target = 97;
+        int[] arr = {1,3,5,2,7,9,6,5,8,7};
+        int target = 7;
 
         System.out.println("Index: "+linearSearch(arr, target));
     }
